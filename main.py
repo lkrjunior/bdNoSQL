fdcf4f86070487b3b42a31791352d80666b3db8d
lkrjunior <lkrjunior@terra.com.br>
2019-10-05 16:06:04 -0300

    Create project

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import configparser
 
 config = configparser.RawConfigParser()
 config.read('config.properties')
-connectionString = config.get('DatabaseSection', 'database.connectionstring')
+connectionString = config.get('DatabaseSection', 'database.connectionString')
 
 print(connectionString)
